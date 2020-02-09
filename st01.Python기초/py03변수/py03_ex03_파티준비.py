0b317f77fef3207e3a9a6810e578c67eeae06f09
Yeji <sapienche@gmail.com>
2020-02-09 18:10:06 +0900

    commit

--- st01.Python기초/py03변수/py03_ex03_파티준비.py
+++ st01.Python기초/py03변수/py03_ex03_파티준비.py
@@ -2,3 +2,15 @@
 # 케익(1인당 4개)를 출력하는 프로그램을 작성해보자.
 
 
+number = int(input ('참석자의 수를 입력하시오 : '))
+chickens = number*1
+beers = number*2
+cakes = number*4
+print("치킨의 수: ", chickens, "마리")
+print("맥주의 수: ", beers, "캔")
+print("케잌의 수: ", cakes, "개")
+
+
+
+
+
