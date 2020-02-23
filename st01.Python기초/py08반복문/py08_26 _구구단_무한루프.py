ef125006285aa5c39e741696e9f3136c1d6e9776
Yeji <sapienche@gmail.com>
2020-02-23 18:11:01 +0900

    20200223 python class

---
+++ st01.Python기초/py08반복문/py08_26 _구구단_무한루프.py
@@ -0,0 +1,21 @@
+
+start = int(input("시작 단수를 입력하세요 : "))
+end = int(input("종료 단수를 입력하세요 : "))
+while True:
+    if start != 0 and end != 0:
+        if start > end:
+            temp = start
+            start = end
+            end = temp
+        else:
+            pass
+        for x in range(start, end+1, 1):
+            for y in range(1, 10, 1):
+                if y == 9:
+                    str = "%2s*%s=%3s" % (x, y, x*y)
+                    print(str, end=".")
+                    print()
+                else:
+                    str = "%2s*%s=%3s" % (x, y, x*y)
+                    print(str, end=",")
+    break
